0654cd20a72073728ae31d3d5d5d74d9f06e9a0b
rlabbiz <83657137+rlabbiz@users.noreply.github.com>
2022-10-27 20:09:59 +0100

    first change

---
+++ ft_strjoin.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rlabbiz <rlabbiz@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/05 13:57:08 by rlabbiz           #+#    #+#             */
+/*   Updated: 2022/10/17 13:35:49 by rlabbiz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*ptr;
+	size_t	i;
+
+	i = 0;
+	if (!s1 || !s2)
+		return (NULL);
+	ptr = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (!ptr)
+		return (NULL);
+	while (*s1 != '\0')
+	{
+		ptr[i] = *s1;
+		i++;
+		s1++;
+	}
+	while (*s2 != '\0')
+	{
+		ptr[i] = *s2;
+		i++;
+		s2++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
