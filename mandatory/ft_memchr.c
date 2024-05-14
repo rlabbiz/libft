19ca670994d8e5d601bf8baabab7eaf51a3c272e
rida labbiz <redalabbiz@gmail.com>
2024-05-14 11:10:48 +0100

    change header file in mandatory part

--- mandatory/ft_memchr.c
+++ mandatory/ft_memchr.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../libft.h"
+#include "../includes/libft.h"
 
 void	*ft_memchr(const void *str, int c, size_t n)
 {
--- mandatory/ft_memset.c
+++ mandatory/ft_memset.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../libft.h"
+#include "../includes/libft.h"
 
 void	*ft_memset(void *ptr, int x, size_t n)
 {
